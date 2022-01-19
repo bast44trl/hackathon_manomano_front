75d7ea696fea00aea0c5f2f0cd3f18a8621fe399
Bastien Traille <b.traille@hotmail.fr>
2022-01-20 00:03:49 +0100

    added background on app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import backgroundLandingPage from "./assets/backgrounds/landing-page.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={backgroundLandingPage} alt="background de mano mano" />
+    </div>
+  );
+}
+
+export default App;
