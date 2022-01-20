119a711bf4bdf3155c31c6cf2765a2066798327b
Bastien Traille <b.traille@hotmail.fr>
2022-01-20 03:08:34 +0100

    emplacement for favorite and purchased icons ready

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -7,9 +7,22 @@ const ProductCard = () => {
         <div className='product-card'>
             <div className='product-card__content'>
                 <img className='product-card__content__img' src={productImg} alt="" />
-                <p className='product-card__content__title'>Titre</p>
+                <p className='product-card__content__title'>La tronçonneuse des mec de la pub Orangina, mais pourquoi sont ils si méchants ?</p>
                 <img className='product-card__content__reviews-img' src={perceuseReviews} alt="" />
-                <p className='product-card__content__price'>56€00</p>
+                <div className='product-card__content__price-favorite'>
+                    <div className='product-card__content__price-favorite__price'>
+                        <div className='product-card__content__price-favorite__price__number'>
+                            56
+                        </div>
+                        <div className='product-card__content__price-favorite__price__euro'>
+                            €
+                        </div>
+                    </div>
+                    <div className='product-card__content__price-favorite__favorite-purchased'>
+                        <div className='product-card__content__price-favorite__favorite-purchased__favorite'></div>
+                        <div className='product-card__content__price-favorite__favorite-purchased__purchased'></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
