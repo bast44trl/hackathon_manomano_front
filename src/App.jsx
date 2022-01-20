18de635840d295db6cd031bf0fa74296e030965c
Louya64 <cixie@live.fr>
2022-01-20 11:09:08 +0100

    add products div in landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import backgroundLandingPage from "./assets/backgrounds/landing-page.png";
-import ProductCard from "./components/ProductCard/ProductCard";
-import './styles/_App.scss';
+// import backgroundLandingPage from "./assets/backgrounds/landing-page.png";
+// import ProductCard from "./components/ProductCard/ProductCard";
+import Products from "./components/Products/Products";
+import "./styles/_App.scss";
 
 function App() {
   return (
-      <div className="App">
-        <div className="App__background">
-          <img src={backgroundLandingPage} alt="background de mano mano" />
-        </div>
-        <ProductCard />
-      </div>
+    <div className="App">
+      <Products />
+      {/* <ProductCard /> */}
+    </div>
   );
+
+  // <div className="App">
+  //   <Products />
+  //   {/* <div className="App__background">
+  //     <img src={backgroundLandingPage} alt="background de mano mano" />
+  //   </div> */}
+  //   <ProductCard />
+  // </div>
 }
 
 export default App;
