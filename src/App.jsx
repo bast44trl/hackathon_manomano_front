8f1330c17a742b078a29b919047e6a25b8ae2c48
Louya64 <cixie@live.fr>
2022-01-20 11:10:53 +0100

    remove comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import backgroundLandingPage from "./assets/backgrounds/landing-page.png";
-// import ProductCard from "./components/ProductCard/ProductCard";
 import Products from "./components/Products/Products";
 import "./styles/_App.scss";
 
@@ -7,17 +5,8 @@ function App() {
   return (
     <div className="App">
       <Products />
-      {/* <ProductCard /> */}
     </div>
   );
-
-  // <div className="App">
-  //   <Products />
-  //   {/* <div className="App__background">
-  //     <img src={backgroundLandingPage} alt="background de mano mano" />
-  //   </div> */}
-  //   <ProductCard />
-  // </div>
 }
 
 export default App;
