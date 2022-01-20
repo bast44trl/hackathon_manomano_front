d1eff39c90a110f4b58a79c8ff2bdf3b0bb9e05c
Bastien Traille <b.traille@hotmail.fr>
2022-01-20 01:29:11 +0100

    corrected sass architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
 import backgroundLandingPage from "./assets/backgrounds/landing-page.png";
 import ProductCard from "./components/ProductCard/ProductCard";
+import './styles/_App.scss';
 
 function App() {
   return (
       <div className="App">
-        <div className="div-background">
+        <div className="App__background">
           <img src={backgroundLandingPage} alt="background de mano mano" />
         </div>
         <ProductCard />
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import './productCard.scss';
 import productImg from '../../assets/productsImg/tronconneuseImg.jpg';
 import perceuseReviews from '../../assets/reviewsImg/perceuseReviews.png';
 
 const ProductCard = () => {
     return (
         <div className='product-card'>
-            <div className='product-card-content'>
-                <img className='product-img' src={productImg} alt="" />
-                <p className='product-title'>Titre</p>
-                <img className='reviews-img' src={perceuseReviews} alt="" />
-                <p className='price'>56€00</p>
+            <div className='product-card__content'>
+                <img className='product-card__content__img' src={productImg} alt="" />
+                <p className='product-card__content__title'>Titre</p>
+                <img className='product-card__content__reviews-img' src={perceuseReviews} alt="" />
+                <p className='product-card__content__price'>56€00</p>
             </div>
         </div>
     );
