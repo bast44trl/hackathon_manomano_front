8d7796c08c31b63e20c3d1b9bd0a8be8ab78edd8
amandine <constans.amandine@gmail.com>
2022-01-20 20:10:55 +0100

    push wishlist

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,28 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import wishlist from '../assets/backgrounds/wishlist.png';
+import "../styles/_wishlist.scss";
+import Products from './Products';
+import ProductCard from './ProductCard';
 
 
 const Wishlist = () => {
-    const [favorite, setFavorite] = useState();
+    const [favorites, setFavorites] = useState();
+let arrayProducts = [];
 
     useEffect(()=>{
         axios
-        .get('localhost:8000/api/lists_products')
-        // .then((response)=> response.data[0])
-        .then((res)=> console.log(res))
-        // .then((data)=> setFavorite(data));
-},[]);
+        .get('http://localhost:8000/api/lists_products/2')
+        .then((res)=> res.data.map((patate)=>
+            axios
+                .get(`http://localhost:8000/api/products/${patate.id_product}`)
+                .then((res)=> arrayProducts.push(res.data))
+                .then(()=> setFavorites(arrayProducts)
+)
+            ))
+    },[]);
+    
+
+
   return (
     <div className="wishlist">
       <h1>My favorites</h1>
       <p>2 products</p>
       <div className='wishlist__containerProduct'>
-          {favorite && 
-            favorite.map((favorite, index)=>(
-                <Wishlist
-                    id={favorite.id}
-                    key={index}
-                    />
+
+          {favorites && 
+            favorites.map((product, index)=>(
+                <ProductCard
+                key={product.id_product}
+                image={require("../assets/productsImg/" + product.picture)}
+                title={product.title}
+                price={product.price}
+              />
             ))
         }
 
